41c734f800ceb2393b43b2534af62d0e689564cd
Greta <gretanembare@gmail.com>
2024-06-12 18:05:15 -0400

    Week5terraformcode

---
+++ code3/output.tf
@@ -0,0 +1,12 @@
+ output "my-public-ip" {
+  value = aws_lightsail_instance.custom.public_ip_address   
+  }
+
+  output "my_username" {
+  value = aws_lightsail_instance.custom.username  
+  }
+
+  output "my_keypair_name" {
+  value = aws_lightsail_instance.custom.key_pair_name
+  }
+ 
